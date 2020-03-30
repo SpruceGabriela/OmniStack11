1b8a8eebda6c8e32cf2503ac14defdb2ea412be3
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2020-03-29 22:21:26 -0300

    initial commit

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const ongController = require('./controllers/ong_controller');
+const incidentController = require('./controllers/incident_controller');
+const profileController = require('./controllers/profile_controller');
+const sessionController = require('./controllers/session_controller');
+
+const routes = express.Router();
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+module.exports = routes;
